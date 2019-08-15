53a6a41baa6db43f28387e4966b549d3572ea9c9
Branden Clark <brandenrayclark@gmail.com>
2019-08-15 09:08:34 -0600

    Initial Commit

---
+++ MapIt-Web-View/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  MapIt-Web-View
+//
+//  Created by Branden Clark on 8/15/19.
+//  Copyright © 2019 Branden Clark. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
